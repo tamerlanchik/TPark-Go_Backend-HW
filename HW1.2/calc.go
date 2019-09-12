08dcf4704968647319d02f975edf8c721586f37f
Ian <andreykochnov@yandex.ru>
2019-09-12 21:21:14 +0300

    Работает с глобальными переменными. Не работает с тестами

--- HW1.2/calc.go
+++ HW1.2/calc.go
@@ -5,35 +5,16 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
-	"os"
 	"strconv"
-	"strings"
 )
 
-var operations = map[byte]int{
-	'(': 0,
-	'+': 1,
-	'*': 2,
-	')': 5,
-}
-
-var numbers = make([]float64, 0)
-var opers = make([]token.Token, 0)
 var level visitor
 
 func main() {
 
-	f, _ := parser.ParseExpr("(1+2)-3")
-	/*ast.Inspect(f, func(n ast.Node) bool {
-		fmt.Println(n)
-		ast.V
-		return true
-	})*/
+	/*f, _ := parser.ParseExpr("(1+2)-3")
 	var v visitor
-	//numbers := make([]float64, 0)
 	ast.Walk(v, f)
-	fmt.Println(numbers)
-	fmt.Println(opers)
 	for len(opers) > 0 {
 		calculate(&numbers, &opers)
 	}
@@ -42,8 +23,17 @@ func main() {
 		fmt.Println("Empty expression")
 		os.Exit(1)
 	}
-	fmt.Println(CalcInterface(os.Args[1]))
+	fmt.Println(CalcInterface(os.Args[1]))*/
+	fmt.Println(CalcInterface("(1+2)-3"))
 }
+
+type Expr struct {
+	operators []token.Token
+	values    []float64
+	maxLevel  visitor
+}
+
+var expr Expr
 
 type visitor int
 
@@ -51,51 +41,32 @@ func (v visitor) Visit(n ast.Node) ast.Visitor {
 	if n == nil {
 		return nil
 	}
-	if v < level {
-		fmt.Println(opers)
-		fmt.Println(numbers)
-		calculate(&numbers, &opers)
+	if v < expr.maxLevel {
+		expr.calculate()
 	}
-	level = v
+	expr.maxLevel = v
 	switch n.(type) {
 	case *ast.BinaryExpr:
 		node := n.(*ast.BinaryExpr)
-		opers = append(opers, node.Op)
-		//fmt.Println("X= " + string(node.X.(type)) + " Y = " + n.Y)
-		/*a := numbers[len(numbers)-1]
-		b := numbers[len(numbers)-2]
-		numbers = numbers[:len(numbers)-2]
-		var result float64*/
-		/*switch node.Op {
-		case token.ADD:
-			fmt.Println("+")
-			//result = a + b
-		case token.SUB:
-			fmt.Println("-")
-			//result = a - b
-		case token.MUL:
-			fmt.Println("*")
-			//result = a * b
-		case token.QUO:
-			fmt.Println("/")
-			//result = a / b
-		default:
-			fmt.Println("?")
-		}*/
-		//numbers = append(numbers, result)
-		//fmt.Print(" " + string(node.Op))
+		expr.operators = append(expr.operators, node.Op)
 	case *ast.BasicLit:
 		node := n.(*ast.BasicLit)
 		value, _ := strconv.ParseFloat(node.Value, 64)
-		numbers = append(numbers, value)
-		fmt.Println(node.Value)
+		expr.values = append(expr.values, value)
 	}
-	fmt.Printf("%s%T\n", strings.Repeat("\t", int(v)), n)
+	//fmt.Printf("%s%T\n", strings.Repeat("\t", int(v)), n)
 	return v + 1
 }
 
 func CalcInterface(expression string) (ans float64) {
+	var v visitor
+	f, _ := parser.ParseExpr(expression)
+	ast.Walk(v, f)
 
+	for len(expr.operators) > 0 {
+		expr.calculate()
+	}
+	ans = expr.values[0]
 	return
 }
 
@@ -104,70 +75,24 @@ func generatePolandNotation(source string) (result []string) {
 	return
 }
 
-func calculate(values *[]float64, operators *[]token.Token) {
-	//vs := *values
-	//opers := *operators
-
-	oper := opers[len(opers)-1]
-	opers = opers[:len(opers)-1]
-	a := numbers[len(numbers)-1]
-	b := numbers[len(numbers)-2]
-	numbers = numbers[:len(numbers)-2]
+func (e *Expr) calculate() {
+	oper := e.operators[len(e.operators)-1]
+	e.operators = e.operators[:len(e.operators)-1]
+	a := e.values[len(e.values)-1]
+	b := e.values[len(e.values)-2]
+	e.values = e.values[:len(e.values)-2]
 	var result float64
 	switch oper {
 	case token.ADD:
-		fmt.Println("+")
 		result = a + b
 	case token.SUB:
-		fmt.Println("-")
 		result = a - b
 	case token.MUL:
-		fmt.Println("*")
 		result = a * b
 	case token.QUO:
-		fmt.Println("/")
 		result = a / b
 	default:
 		fmt.Println("?")
 	}
-	numbers = append(numbers, result)
-
-	//*values = vs
-	//*operators = opers
+	e.values = append(e.values, result)
 }
-
-/*
-func calculateExpression(expression []string) (answer float64, resultErr error) {
-	var stack = make([]string, 0, len(expression))
-
-	for _, val := range expression {
-		if _, err := strconv.ParseFloat(val, 64); err == nil {
-			stack = append(stack, val)
-			continue
-		}
-
-		a, _ := strconv.ParseFloat(stack[len(stack)-1], 64)
-		b, _ := strconv.ParseFloat(stack[len(stack)-2], 64)
-		stack = stack[:len(stack)-2]
-
-		var result float64
-		switch val {
-		case "+":
-			result = a + b
-		case "-":
-			result = a - b
-		case "*":
-			result = a * b
-		case "/":
-			result = a / b
-		default:
-			resultErr = fmt.Errorf("Wrong operator")
-			return
-		}
-		fmt.Println(result)
-		//stack = append(stack, string(result))
-
-	}
-	return
-}
-*/
